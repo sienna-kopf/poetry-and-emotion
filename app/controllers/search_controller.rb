f8d2bcc1969e6c918e2c67265dbfff99810ada99
Sienna Kopf <princess.kopf@gmail.com>
2020-09-11 09:39:54 -0600

    Dream drive controller functionality for searching poems by author

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    author = params[:author]
+    search_facade = SearchFacade.new(author)
+    @poems = search_facade.search_author
   end
 end

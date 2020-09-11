179e7886dba6e284e2310b1d96d70bf797523832
Sienna Kopf <princess.kopf@gmail.com>
2020-09-11 10:31:49 -0600

    Build out tone poro for grabbing tones

---
+++ app/poros/tone.rb
@@ -0,0 +1,14 @@
+class Tone
+  attr_reader :tone_list
+
+  def initialize(tone_info)
+    @tone_list = tone_info[:document_tone][:tones]
+    @tones = []
+  end
+
+  def tones
+    @tone_list.map do |tone|
+      @tones << tone[:tone_name]
+    end.flatten
+  end
+end
